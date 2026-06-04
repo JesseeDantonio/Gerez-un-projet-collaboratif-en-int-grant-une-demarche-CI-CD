6204fa595bdab01b006453ab85f1c21fc6871a28
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-06-04 12:24:27 +0200

    feat: insertion d'un message console pour test ci backend

--- back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
+++ back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BobappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Bobapp!");
 		SpringApplication.run(BobappApplication.class, args);
 	}
 
