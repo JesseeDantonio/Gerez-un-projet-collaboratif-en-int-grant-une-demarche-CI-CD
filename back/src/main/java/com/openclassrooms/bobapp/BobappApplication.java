ca46a24b58f8c8c1eddf7e39953a122a8bd265b0
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-06-04 12:50:22 +0200

    fix: suppression d'un message console pour test ci backend

--- back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
+++ back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BobappApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Bobapp!");
 		SpringApplication.run(BobappApplication.class, args);
 	}
 
