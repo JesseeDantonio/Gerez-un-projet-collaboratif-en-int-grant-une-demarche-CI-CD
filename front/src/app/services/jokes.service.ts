dcba6a3a59282b7c2e9cf9eab1654e874e6807b8
JesseeDantonio <dyland.antonio1998@gmail.com>
2026-06-05 09:07:53 +0200

    feat(): ajout d'une méthode avec aucun test

--- front/src/app/services/jokes.service.ts
+++ front/src/app/services/jokes.service.ts
@@ -23,4 +23,16 @@ export class JokesService {
   public joke$(): Observable<Joke | null > {
     return this.subject.asObservable();
   }
+
+   public getJokeByCategory(category: string): string {
+    if (category === 'dev') {
+      return "Pourquoi les développeurs détestent la nature ? Parce qu'il y a trop de bugs.";
+    } else if (category === 'network') {
+      return "Je raconterais bien une blague sur l'UDP, mais je ne suis pas sûr que tu la reçoives.";
+    } else if (category === 'database') {
+      return "Un admin BDD rentre dans un bar et demande à deux tables : 'Je peux me joindre à vous ?'";
+    } else {
+      return "Pas de blague pour cette catégorie.";
+    }
+  }
 }
